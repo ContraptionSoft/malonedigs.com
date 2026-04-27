82ba038fcf1fc835ea58575ebe50c00756bb568c
Tyler Malone <tyler@vps.contraptionsoft.com>
2026-04-27 01:30:23 +0000

    Use simplified logo mark in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
           {/* Logo */}
           <Link href="/" className="flex items-center gap-3 shrink-0">
             <Image
-              src="/logo.png"
+              src="/logo-mark.png"
               alt="Malone Excavation"
               width={48}
               height={48}
